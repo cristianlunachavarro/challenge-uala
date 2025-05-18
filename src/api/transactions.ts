4815379d8cc2f2888055b95719660173f59bc7cf
Cristian Luna <cristianluna@Cristians-MacBook-Pro-2.local>
2025-05-18 19:49:03 -0300

    Added transactions Json file as a static asset

--- src/api/transactions.ts
+++ src/api/transactions.ts
@@ -1,23 +1,12 @@
-import axios from 'axios';
+import Transactions from '../../public/transactions.json';
 import type { Transaction, Metadata } from '../types/transactions';
 
 interface ApiResponse {
   transactions: Transaction[];
   metadata: Metadata;
 }
-const vercelEnv = import.meta.env.VITE_VERCEL_ENV;
-
-const API_URL =
-  vercelEnv === 'production'
-    ? 'https://uala-dev-challenge.s3.us-east-1.amazonaws.com/transactions.json'
-    : '/api/transactions';
 
 export const fetchTransactions = async (): Promise<ApiResponse> => {
-const response = await axios.get(API_URL, {
-  headers: {
-    Origin: 'https://challenge-uala-inky.vercel.app',
-    'Access-Control-Request-Headers': 'Content-Type',
-  },
-})  
-return response.data;
+  const response = await Promise.resolve(Transactions);
+  return response;
 };
