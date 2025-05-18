1980cd6eb36b86e86bd913760d31cdf9a9c7732c
Cristian Luna <cristianluna@Cristians-MacBook-Pro-2.local>
2025-05-18 13:06:46 -0300

    Fixed API URL and fetch function to work with vercel in prod - working on

--- src/api/transactions.ts
+++ src/api/transactions.ts
@@ -1,16 +1,12 @@
 import axios from 'axios';
-
-import { Transaction, Metadata } from '../types/transactions';
+import type { Transaction, Metadata } from '../types/transactions';
 
 interface ApiResponse {
   transactions: Transaction[];
   metadata: Metadata;
 }
 
-const API_URL =
-  import.meta.env.MODE === 'development'
-    ? '/api/transactions'
-    : 'https://uala-dev-challenge.s3.us-east-1.amazonaws.com/transactions.json';
+const API_URL = '/api/transactions';
 
 export const fetchTransactions = async (): Promise<ApiResponse> => {
   const response = await axios.get(API_URL);
